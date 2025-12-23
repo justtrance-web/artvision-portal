8aa35a9606f35efc80ea913e7056d41848fc7be6
justtrance-web <justtrance@gmail.com>
2025-12-23 06:19:43 +0300

    Add app/api/clients/route.ts

---
+++ app/api/clients/route.ts
@@ -0,0 +1,15 @@
+import { supabase } from "@/lib/supabase"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  const { data, error } = await supabase
+    .from("clients")
+    .select("*")
+    .order("status")
+  
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+  
+  return NextResponse.json(data)
+}
